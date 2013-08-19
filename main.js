f07a2249719b61dc35f819af72e507dfa525a0ff
Roman Levin <roman.levin@stxnext.pl>
2013-08-19 15:16:58 +0200

    Basic functionality.  - Adds an input field below ticket comments.  - When submitting a comment appends an Apropos line to comment,    using the value of the new input field as the number of hours.

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+/*jshint indent:4 */
 /*globals HTML */
+(function() {
+    'use strict';
+    var inputNodeDesc = 'input#apropos-hours';
+    var form = HTML.find('div.write-content');
+    form.add(inputNodeDesc);
 
-var textareas = HTML.find('div.write-content');
-textareas.add('input');
-// console.log(textareas);
+    var textarea = HTML.find('textarea[name="comment[body]"]');
+
+    function addAproposListener(el) {
+        el.addEventListener('click', function() {
+            var hours = HTML.find(inputNodeDesc).value;
+            textarea.each(function(el) {
+                var addition = '\nSpent ' + hours + ' hours on this.';
+                el.value += addition;
+            });
+        });
+    }
+
+    var submit = HTML.find('div.form-actions button[type="submit"]');
+    submit.each(addAproposListener);
+})();
